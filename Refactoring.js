bfee24a5f5a792885e58cf13c3ce5791ff386dc7
pdjigne <132499480+pdjigne@users.noreply.github.com>
2023-11-30 23:31:26 -0500

    Add files via upload

---
+++ Refactoring.js
@@ -0,0 +1,77 @@
+// Code to parse CSVs
+function parseCSV(csvData) {
+    // Implementation goes here
+  }
+//   Working with Arrays
+
+
+// Code to work with arrays
+const numbers = [1, 2, 3, 4, 5];
+
+// Accessing elements
+const firstElement = numbers[0];
+
+// Modifying elements
+numbers[2] = 10;
+
+// Adding elements
+numbers.push(6);
+
+// Removing elements
+numbers.pop();
+
+
+// Code to work with objects
+const person = {
+  name: "John Doe",
+  age: 30,
+  profession: "Developer"
+};
+
+// Accessing properties
+const name = person.name;
+
+// Modifying properties
+person.age = 31;
+
+// Adding properties
+person.location = "New York";
+
+// Removing properties
+delete person.profession;
+// Code to use conditional logic
+const age = 18;
+
+if (age >= 18) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are a minor.");
+}
+// Code to use loops
+const numbers = [1, 2, 3, 4, 5];
+
+// For loop
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+
+// While loop
+let i = 0;
+while (i < numbers.length) {
+  console.log(numbers[i]);
+  i++;
+}
+// Code to transform data
+const numbers = [1, 2, 3, 4, 5];
+
+const doubledNumbers = numbers.map((number) => number * 2);
+
+const filteredNumbers = numbers.filter((number) => number > 3);
+
+const sum = numbers.reduce((accumulator, number) => accumulator + number, 0);
+
+
+
+
+
+
